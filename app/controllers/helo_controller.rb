bc03ead7f3284110bafdd4c71810de0185b10761
hironorim <hironori.m@gmail.com>
2017-06-13 00:10:51 +0900

    作業中のをコミット

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,5 +1,21 @@
 class HeloController < ApplicationController
+    layout 'helo'
+    
     def index
-        render plain:"Hello. Thies is Rails sample page!"
+        @header = 'layout sample'
+        @footer = 'copyright hogehoge'
+        if request.post? then
+            @title = 'Result'
+            if params['check1'] then
+                @msg = 'you Checked!!'
+            else
+                @msg = 'not Checked...'
+            end
+            @inputValue = params['input1']
+            @yourOS = 'Your OS is' + params['select1']
+        else
+            @title = 'Index'
+            @msg = 'checked it...'
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,58 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+
+  get 'blogs/genre'
+
+  get 'blogs/show'
+
+  get 'blogposts/index'
+
+  get 'blogposts/add'
+
+  get 'blogposts/edit'
+
+  get 'blogposts/delete'
+
+  get 'bloggenres/index'
+
+  get 'bloggenres/add'
+
+  get 'bloggenres/edit'
+
+  get 'blogconfigs/index'
+
+  get 'blogconfigs/edit'
+
+  get 'cards/index'
+
+  get 'cards/show'
+
+  get 'cards/add'
+
+  get 'cards/edit'
+
+  get 'dengonban/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'helo/index'
+  post 'helo/index'
   get 'helo', to: 'helo#index'
+
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+
+  get 'cards/delete/:id', to: 'cards#delete'
 end
